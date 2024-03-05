b9fee46641092fe13dd934334755a148b97e3a1b
Adam-board <adamboard2003@gmail.com>
2024-03-04 18:53:46 -0800

    implemented Buttons being disabled when not required

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,7 @@
+
+//If making a comment inside of React Fragment, use this {/* */}
+
+
 //The main imports to ensure everything is treated as React 
 import * as React from 'react';
 import Grid from '@mui/material/Grid'; // Grid version 1
@@ -11,6 +15,7 @@ import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import PropTypes from 'prop-types';
+import Tooltip from '@mui/material/Tooltip';
 
 //Objects that handle the different tabs which exist
 import Tabs from '@mui/material/Tabs';
@@ -29,50 +34,149 @@ import EditIcon from '@mui/icons-material/Edit'; //Edit Button
 import ImportExportIcon from '@mui/icons-material/ImportExport'; //Import Export Button
 import CoronavirusIcon from '@mui/icons-material/Coronavirus'; //Vuln Tab
 import BookmarksIcon from '@mui/icons-material/Bookmarks'; //Bookmarks Tab
-import { blue } from '@mui/material/colors';
 
 
 
 //The variables where all the buttons related to reports are declared 
 //and can be interacted with at a later point using keys
 
+//Report Options Button Descriptions
+const NewReport = `Begin a new report with the default template of section headings displayed automatically and ready to edit. 
+                  Saving a new default report template will overrite the one that is currently written in when a new report is started. 
+                  If you have unsaved changes these will not be saved so be careful as you could lose a lot of work!`
+
+const LoadReport = `Load a previously worked on report to continue from when it was last saved.`
+const SaveReport = `Save all the hard work put into the report. Make sure to save regularly!`
+const LoadReportTemplate = `Load a template of headings that have been created and saved previously.`
+const SaveReportTemplate = `Save your current template so it can be loaded again for later use!`
+const SaveDefaultTemplate = `Save your currrent template as the default template when "New Report" is pressed!`
+
+//Section Options Button Descriptions
+const AddSectionHeading = `Add a new section heading into the report. This includes main headings such as Executive summary or any subsections that may be wanted by the user!`
+const RemoveSectionHeading = `Remove an existing section heading.`
+const EditSectionHeading = `Edit the name of the heading as well as the type of heading (main-heading or sub-heading).`
+
+//Vulnerability Options Button Descriptions
+const AddVulnToReport = `Add a new vulnerability to the report, make sure to fill in the description, severity, steps to reproduce, and mitigation.`
+const RemoveVulnFromReport = `Remove a vulnerability from a report.`
+const RemoveVulnFromDatabank = `Remove a vulnerability from the databank.`
+const ImportVulnDatabank = `Import a list of already created vulnerability descriptions and severity ratings from one server to another server!`
+const AddVulnToDatabank = `Add a new vulnerability to the databank by filling in the required fields that pop-up.`
+const InsertVulnFromDatabank = `Add a vulnerability to the report directly from the databank to speed up the process!`
+const LoadVulnDatabank = `Load a previously written databank that is contained within storage of the server.`
+const SaveVulnDatabank = `Save the currently working vulnerability databank onto the server's storage to be used at a later date.`
+
+//Audit Options Button Descriptions
+const AddAuditEntry = `Add an entry of the tool that was used to discover a specific vulnerability and link it to a vulnerability that is inside of the report currently.`
+const RemoveAuditEntry = `Remove an audit entry.`
+const EditAuditEntry = `Edit an audit entry.`
+const ImportAuditLog = `Import the audit log of another report into the report currently being worked on.`
+
+//Buttons in Toolbar
+const ExportVulnToJira = `Export all of the vulnerabilities into a list of tickets that work inside of a Jira board, this can then be imported into other organisation's Jira boards. Each ticket is categorised by its severity raiting. `
+const ExportReport = `Export all of the sections and vulnerabilities into a PDF that can be reviewed and downloaded by the user. The report will use a default styling for when the report is exported, this will be changed in future!`
+
+
+
+
+
+
+
+
+
+
+
+
+
+const ButtonWidth = 370
+
+
 //TODO implement functionality into buttons based on their respective keys
 const ButtonsReport = [
-  <Button key="New Report" startIcon={<DriveFolderUploadIcon />}>New Report</Button>,
-  <Button key="Load Report" startIcon={<DriveFolderUploadIcon />}>Load Report</Button>,
-  <Button key="Save Report" startIcon={<SaveIcon />} >Save Report</Button>,
-  <Button key="Load Report Template" startIcon={<DriveFolderUploadIcon  />}>Load Report Template</Button>,
-  <Button key="Save Report Template" startIcon={<SaveIcon />} >Save Report Template</Button>,
-  <Button key="Set Default Report Template" startIcon={<DescriptionIcon />}>Set Default Report Template</Button>,
+  <Tooltip title={NewReport} arrow placement='right-start'>
+  <Button key="New Report" startIcon={<DriveFolderUploadIcon />}>New Report</Button>
+  </Tooltip>,
+  <Tooltip title={LoadReport} arrow placement='right'>
+  <Button key="Load Report" startIcon={<DriveFolderUploadIcon />}>Load Report</Button> 
+  </Tooltip>,
+  <Tooltip title={SaveReport} arrow placement='right'>
+  <Button key="Save Report" startIcon={<SaveIcon />} >Save Report</Button>
+  </Tooltip>,
+  <Tooltip title={LoadReportTemplate} arrow placement='right'>
+  <Button key="Load Report Template" startIcon={<DriveFolderUploadIcon  />}>Load Report Template</Button>
+  </Tooltip>,
+  <Tooltip title={SaveReportTemplate} arrow placement='right'>
+  <Button key="Save Report Template" startIcon={<SaveIcon />} >Save Report Template</Button>
+  </Tooltip>,
+  <Tooltip title={SaveDefaultTemplate} arrow placement='right-end'>
+  <Button key="Set Default Report Template" startIcon={<DescriptionIcon />}>Set Default Report Template</Button> 
+  </Tooltip>,
 ];
 
 
 const SectionHeading = [
- 
-  <Button key="Add Heading" startIcon={<AddCircleOutlineIcon />}>Add Section Heading</Button>,
-  <Button key="Remove Heading" startIcon={<DeleteIcon />} >Remove Section Heading</Button>,
-  <Button key="Edit Heading" startIcon={<EditIcon />}>Edit Section Heading</Button>,
-  <Button key="Import Heading" startIcon={<ImportExportIcon  />}>Import Section Headings</Button>,
+  <Tooltip title={AddSectionHeading} arrow placement='right'>
+  <Button key="Add Heading" startIcon={<AddCircleOutlineIcon />}>Add Section Heading</Button>
+  </Tooltip>,
+  <Tooltip title={RemoveSectionHeading} arrow placement='right'>
+  <Button key="Remove Heading" startIcon={<DeleteIcon />} >Remove Section Heading</Button>
+  </Tooltip>,
+  <Tooltip title={EditSectionHeading} arrow placement='right'>
+  <Button key="Edit Heading" startIcon={<EditIcon />}>Edit Section Heading</Button>
+  </Tooltip>,
 ];
 
 const ButtonsVulnDatabank = [
-  <Button key="Load Vulns" startIcon={<ImportExportIcon  />}>Import Vulnerability Databank</Button>,
-  <Button key="Add Vulns" startIcon={<AddCircleOutlineIcon />}>Add New Vulnerability To Databank</Button>,
-  <Button key="Add Vulns" startIcon={<AddCircleOutlineIcon />}>Insert Vulnerability From Databank</Button>,
-  <Button key="Load Vuln" startIcon={<DriveFolderUploadIcon />}>Load Vulnerability Databank</Button>,
+  <Tooltip title={AddVulnToReport} arrow placement='right'>
+  <Button key="Add Vulns To Report" startIcon={<AddCircleOutlineIcon />}>Add Vulnerability To Report</Button>
+  </Tooltip>,
+  <Tooltip title={RemoveVulnFromReport} arrow placement='right'>
+  <Button key="Remove Vuln" startIcon={<DeleteIcon />}>Remove Vulnerability From Report</Button>
+  </Tooltip>,
+  <Tooltip title={AddVulnToDatabank} arrow placement='right'>
+  <Button key="Add Vuln to Databank" startIcon={<AddCircleOutlineIcon />}>Add New Vulnerability To Databank</Button>
+  </Tooltip>,
+  <Tooltip title={InsertVulnFromDatabank} arrow placement='right'>
+  <Button key="Insert Vulns From Databank" startIcon={<AddCircleOutlineIcon />}>Insert Vulnerability From Databank</Button>
+  </Tooltip>,
+  <Tooltip title={RemoveVulnFromDatabank} arrow placement='right'>
+  <Button key="Remove Vuln From Databank" startIcon={<DeleteIcon />}>Remove Vulnerability From Databank</Button>
+  </Tooltip>,
+  <Tooltip title={LoadVulnDatabank} arrow placement='right'>
+  <Button key="Load Vuln" startIcon={<DriveFolderUploadIcon />}>Load Vulnerability Databank</Button>
+  </Tooltip>,
+  <Tooltip title={SaveVulnDatabank} arrow placement='right'>
   <Button key="Save Vuln" startIcon={<SaveIcon />} >Save Vulnerability Databank</Button>
+  </Tooltip>,
+  
+   <Tooltip title={ImportVulnDatabank} arrow placement='right'>
+  <Button key="Load Vulns" startIcon={<ImportExportIcon  />}>Import Vulnerability Databank</Button>
+  </Tooltip>,
 ];
 
 const AuditLog = [
-  <Button key="Add Audit" startIcon={<AddCircleOutlineIcon />}>Add Audit Entry</Button>,
-  <Button key="Remove Audit" startIcon={<DeleteIcon />}>Remove Audit Entry</Button>,
-  <Button key="Edit Audit" startIcon={<EditIcon />}>Edit Audit Entry</Button>,
-  <Button key="Import Audit" startIcon={<ImportExportIcon />}>Import Audit Log</Button>,
+  <Tooltip title={AddAuditEntry} arrow placement='right'>
+  <Button key="Add Audit" startIcon={<AddCircleOutlineIcon />}>Add Audit Entry</Button>
+  </Tooltip>,
+  <Tooltip title={RemoveAuditEntry} arrow placement='right'>
+  <Button key="Remove Audit" startIcon={<DeleteIcon />}>Remove Audit Entry</Button>
+  </Tooltip>,
+  <Tooltip title={EditAuditEntry} arrow placement='right'>
+  <Button key="Edit Audit" startIcon={<EditIcon />}>Edit Audit Entry</Button>
+  </Tooltip>,
+  <Tooltip title={ImportAuditLog} arrow placement='right'>
+  <Button key="Import Audit" startIcon={<ImportExportIcon />}>Import Audit Log</Button>
+  </Tooltip>,
 ];
 
-const ComplianceFlag = { inputProps: { 'aria-label': 'Checkbox demo' } };
 
-//This handles the tabs which will appear for teach section
+//This section contains all the tool tips to understand what each button and tab does
+
+
+//Checkbox for Marking Stuff
+//const ComplianceFlag = { inputProps: { 'aria-label': 'Checkbox demo' } };
+
+//This handles the tabs which will appear for each section
 function CustomTabPanel(props) {
   const { children, value, index, ...other } = props;
 
@@ -126,8 +230,17 @@ function App() {
       <AppBar position='sticky'>
       <Toolbar variant='dense'>
       <Typography variant='h6' component='div' fontStyle={'italic'} sx={{ flexGrow: 1}}>Report Writing Tool</Typography>
+
+
+      |<Tooltip title={ExportVulnToJira} arrow placement='bottom-end'>
       <Button key="Export Vulns to Jira" startIcon={<FileDownloadIcon />} variant='contained' disableElevation >Export Vulnerabilities To Jira</Button>
+      </Tooltip>
+
+      <Tooltip title={ExportReport} arrow placement='bottom-start'>
       <Button key="Export Report" startIcon={<FileDownloadIcon />} variant='contained' disableElevation >Export Report (PDF)</Button>
+      </Tooltip>
+
+
       
       </Toolbar>
       </AppBar>
@@ -148,21 +261,32 @@ function App() {
 <Box sx={{bgcolor: 'background.paper', display: 'inline-flex', flexDirection: 'row', alignContent: 'stretch' , justifyContent: 'flex-start', flexWrap: 'nowrap', width: '100%'}}>
 
 <Box sx={{bgcolor: 'ActiveCaption', justifyContent: 'center', alignItems: 'center'}}>
-      <Grid item width={355}>
-      <ButtonGroup
+      
+      
+      
+      <Grid item width={ButtonWidth}>
+      <ButtonGroup 
+          onChange={handleChange}
+          disabled={value !==  0}
           orientation="vertical"
           aria-label="Vertical button group"
           variant="contained"
           disableElevation
-          fullWidth>
+          fullWidth
+          >
           
           <Typography variant='h7' margin={1} fontStyle={'italic'} component='div' sx={{ flexGrow: 1}}>Report Options</Typography>
           {ButtonsReport}
         </ButtonGroup>
         </Grid>
 
-        <Grid item width={355}>
+
+
+
+        <Grid item width={ButtonWidth}>
         <ButtonGroup
+          onChange={handleChange}
+          disabled={value  !== 0}
           orientation="vertical"
           aria-label="Vertical button group"
           variant="contained"
@@ -174,8 +298,10 @@ function App() {
           
         </ButtonGroup>
 </Grid>
-<Grid item width={355}>
+<Grid item width={ButtonWidth}>
         <ButtonGroup
+          onChange={handleChange}
+          disabled={value !== 1}
           orientation="vertical"
           aria-label="Vertical button group"
           variant="contained"
@@ -187,8 +313,13 @@ function App() {
   
         </ButtonGroup>
         </Grid>
-        <Grid item width={355}> 
+
+
+
+        <Grid item width={ButtonWidth}> 
         <ButtonGroup
+          onChange={handleChange}
+          disabled={value !== 2}
           orientation="vertical"
           aria-label="Vertical button group"
           variant="contained"
