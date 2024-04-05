e5a38e78a35e5c9732e1e11221b243bf6cf4decf
Adam-board <adamboard2003@gmail.com>
2024-04-05 08:04:57 -0700

    Before Changing to component-based programming (properly)

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,10 @@
 
-//If making a comment inside of React Fragment, use this {/* */}
-
+//If making a comment inside of the React Fragment, use this {/* */}
 
 //The main imports to ensure everything is treated as React 
 import * as React from 'react';
-import Grid from '@mui/material/Grid'; // Grid version 1
 
-//All objects related to this project
+//All objects related to this project From MUI
 import Button from '@mui/material/Button';
 import ButtonGroup from '@mui/material/ButtonGroup';
 import Checkbox from '@mui/material/Checkbox';
@@ -16,6 +14,12 @@ import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import PropTypes from 'prop-types';
 import Tooltip from '@mui/material/Tooltip';
+import TextField from '@mui/material/TextField';
+import DialogTitle from '@mui/material/DialogTitle';
+import Dialog from '@mui/material/Dialog';
+import IconButton from '@mui/material/IconButton';
+import Fab from '@mui/material/Fab';
+import Grid from '@mui/material/Grid'; // Grid version 1
 
 //Objects that handle the different tabs which exist
 import Tabs from '@mui/material/Tabs';
@@ -36,9 +40,13 @@ import CoronavirusIcon from '@mui/icons-material/Coronavirus'; //Vuln Tab
 import BookmarksIcon from '@mui/icons-material/Bookmarks'; //Bookmarks Tab
 
 
+//Contains all imports for Draft.Js
+import ReactDOM from 'react-dom';
+import {Editor, EditorState} from 'draft-js';
+import 'draft-js/dist/Draft.css';
 
-//The variables where all the buttons related to reports are declared 
-//and can be interacted with at a later point using keys
+
+//This section contains all the tool tips to understand what each button and tab does
 
 //Report Options Button Descriptions
 const NewReport = `Begin a new report with the default template of section headings displayed automatically and ready to edit. 
@@ -79,17 +87,8 @@ const ExportReport = `Export all of the sections and vulnerabilities into a PDF 
 
 
 
-
-
-
-
-
-
-
-
-
+//The buttongroups and all objects/variables related to buttons.
 const ButtonWidth = 370
-
 
 //TODO implement functionality into buttons based on their respective keys
 const ButtonsReport = [
@@ -170,7 +169,11 @@ const AuditLog = [
 ];
 
 
-//This section contains all the tool tips to understand what each button and tab does
+//<IconButton aria-label='delete' color='error' ><DeleteIcon /> </IconButton>
+
+
+//<Editor editorState={editorState} onChange={setEditorState}/>
+        
 
 
 //Checkbox for Marking Stuff
@@ -204,7 +207,6 @@ CustomTabPanel.propTypes = {
   value: PropTypes.number.isRequired,
 };
 
-
 function a11yProps(index) {
   return {
     id: `simple-tab-${index}`,
@@ -213,14 +215,23 @@ function a11yProps(index) {
 }
 
 
+
 //TODO Implement all objects required to finish UI in design phase
 //TODO Plan out where the layout will go and how it will look
 function App() {
+
+
+ // Creates a new editor instance of DraftJS. (Use for each section that is added into the report to allow as much customisability as possible!)
+ //const [editorState, setEditorState] = React.useState(
+  //() => EditorState.createEmpty(), );
+ 
+
   const [value, setValue] = React.useState(0);
 
-  const [] = React.useState('');
-  const [] = React.useState('');
-  const [] = React.useState('');
+  //These will create a new entry everytime it is created through clicking the appropriate button (e.g. Add Section Heading will open a popup, fill out details and press save, it will then display and can be written to)
+  const [HeadingEntry, setHeadingEntry] = React.useState('');
+  const [VulnEntry, setVulnEntry] = React.useState('');
+  const [AuditEntry, setAuditEntry] = React.useState('');
 
 
   const handleChange = (event, newValue) => {
@@ -245,7 +256,6 @@ function App() {
       </Tooltip>
 
 
-      
       </Toolbar>
       </AppBar>
     </Box>
@@ -264,7 +274,7 @@ function App() {
     </Grid>
 <Box sx={{bgcolor: 'background.paper', display: 'inline-flex', flexDirection: 'row', alignContent: 'stretch' , justifyContent: 'flex-start', flexWrap: 'nowrap', width: '100%'}}>
 
-<Box sx={{bgcolor: 'ActiveCaption', justifyContent: 'center', alignItems: 'center'}}>
+<Box sx={{justifyContent: 'center', alignItems: 'center'}}>
       
       
       
@@ -337,26 +347,119 @@ function App() {
         </Box>
         
         
-     <Box sx={{bgcolor: 'ButtonHighlight', justifyContent: 'center', alignItems: 'center',  width: '85%', marginLeft: 1}}>
+     <Box sx={{bgcolor: '#e6e6e6', border:1, width: '84%', height: '100%', marginLeft: 1}}>
+
      <CustomTabPanel value={value} index={0}>
-      This Section allows the user to write up sections of the report 
+
+     <Grid container
+         margin={1}
+         direction={'column'}
+         marginLeft={"40%"}>
+    
+
+    <Grid item>
+     <Box
+        component='form'
+        sx={{  '& > :not(style)': { m: 1, width: '50' },}}
+        noValidate
+        autoComplete="off"
+        alignContent="center">
+
+        <TextField id="Heading" label="Heading" variant='outlined' />
+        
+        <Fab color="primary" aria-label="add" marginLeft={"40%"} Type="submit">
+        <AddCircleOutlineIcon />
+      </Fab>
+
+        </Box>
+
+        </Grid>
+       
+      </Grid>     
+
       
+
       </CustomTabPanel>
       
+
+
       <CustomTabPanel value={value} index={1}>
-        This section allows the user to insert Vulnerabilities from either the vulnerability databank or write the vulnerabilities new themselves
+
+      <Grid container
+         margin={1}
+         direction={'column'}
+         marginLeft={"40%"}>
+    
+
+    <Grid item>
+     <Box
+        component='form'
+        sx={{  '& > :not(style)': { m: 1, width: '50' },}}
+        noValidate
+        autoComplete="off"
+        alignContent="center">
+
+        <TextField id="Vulnerability" label="Vulnerability" variant='outlined' />
+        
+        
+
+        </Box>
+
+        </Grid>
+        <Grid item marginLeft={"5%"}>
+        <Fab color="primary" aria-label="add" marginLeft={"40%"}>
+        <AddCircleOutlineIcon />
+      </Fab>
+      </Grid>     
+
+      </Grid>
       </CustomTabPanel>
      
      <CustomTabPanel value={value} index={2}>
-        This section is an audit log that can be attached to the vulnerabilities so an external user could see the tools used to discover the vulnerability
+
+
+     <Grid container
+         margin={1}
+         direction={'column'}
+         marginLeft={"40%"}>
+    
+
+    <Grid item>
+     <Box
+        component='form'
+        sx={{  '& > :not(style)': { m: 1, width: '50' },}}
+        noValidate
+        autoComplete="off"
+        alignContent="center">
+
+        <TextField id="AuditEntry" label="Audit Entry" variant='outlined' />
+
+        
+        
+        
+
+        </Box>
+
+        </Grid>
+        <Grid item marginLeft={"5%"}>
+        <Fab color="primary" aria-label="add" marginLeft={"40%"}>
+        <AddCircleOutlineIcon />
+      </Fab>
+      </Grid>     
+
+      </Grid>
       </CustomTabPanel>
 
       </Box>
         </Box>
           </Grid>
           
+
+
+          
         </React.Fragment>
 
   );
 }
+
 export default App 
