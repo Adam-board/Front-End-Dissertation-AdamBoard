3eed9006bd33c26d1d8efbb935c23a6aad66a5fd
Adam-board <adamboard2003@gmail.com>
2024-04-09 16:41:22 -0700

    Shortened some code and prepping the code to be changed to use Router

--- client/src/App.js
+++ client/src/App.js
@@ -1,72 +1,30 @@
 //The main imports to ensure everything is treated as React 
 import * as React from 'react';
 import Grid from '@mui/material/Grid'; // Grid version 1
-import { IconButton} from '@mui/material';
 import {BrowserRouter, Routes, Route, Link} from "react-router-dom";
 
-//All icons that are being imported for this project
-import SaveIcon from '@mui/icons-material/Save'; //Save report
-import DriveFolderUploadIcon from '@mui/icons-material/DriveFolderUpload'; //Load Report
-import DescriptionIcon from '@mui/icons-material/Description'; //Set Template & Load Template
-import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline'; // Add Vulns
-import DeleteIcon from '@mui/icons-material/Delete'; //Remove Button
-import ImportExportIcon from '@mui/icons-material/ImportExport'; //Import Export Button
-
 //Custom components imported to make everything neater
-
 //Components related to the ToolBar
 import CustToolBarButtons from './Components/ToolBar/CustToolBarButtons';
 import CustToolBar from './Components/ToolBar/CustToolBar';
 //Components related to the SideBar
 import CustButtonGroup from './Components/SideBar/ButtonGroups';
-import CustButton from './Components/SideBar/CustButton';
 //Components related to the Tabs
 import CustTabPanel from './Components/Tabs/TabPanel';
 import CustTabs from './Components/Tabs/CustTabs';
 import CustForm from './Components/Tabs/CustForm';
+import CustAutoComplete from './Components/SideBar/AutoComplete';
 //Components related to the Editor Page
 import CustSection from './Components/Editor/Section';
-import CustRenderEditor from './Components/Editor/RenderEditor';
-
-//Contains all imports for Draft.Js
-import ReactDOM from 'react-dom';
-import {Editor, EditorState} from 'draft-js';
-import 'draft-js/dist/Draft.css';
-
+//Import all buttons used on sidebar
+import * as Side from './Components/SideBar/DataButtonGroups'
 //Import of all ToolTip Descriptions
 import * as Desc from './Language/ToolTipDesc'
 
-//TODO implement functionality into buttons based on their respective keys
-const ButtonsReport = [
-<CustButton  startIcon={<DriveFolderUploadIcon />} tooltip={Desc.NewReport} arrowPlacement="right-start" >New Report</CustButton>,
-<CustButton  startIcon={<DriveFolderUploadIcon />} tooltip={Desc.LoadReport} arrowPlacement="right" >Load Report</CustButton>,
-<CustButton  startIcon={<SaveIcon/>} tooltip={Desc.SaveReport} arrowPlacement="right" >Save Report</CustButton>,
-<CustButton  startIcon={<DriveFolderUploadIcon />} tooltip={Desc.LoadReportTemplate} arrowPlacement="right" >Load Report Template</CustButton>,
-<CustButton  startIcon={<SaveIcon/>} tooltip={Desc.SaveReportTemplate} arrowPlacement="right-end" >Save Report Template</CustButton>,
-<CustButton  startIcon={<DescriptionIcon/>} tooltip={Desc.SaveDefaultTemplate} arrowPlacement="right-start" >Set Default Report Template</CustButton>
-];
-
-const ButtonsVulnDatabank = [
-<CustButton  startIcon={<AddCircleOutlineIcon />} tooltip={Desc.InsertVulnFromDatabank} arrowPlacement="right" >Insert Vulnerability From Databank</CustButton>,
-<CustButton  startIcon={<DeleteIcon/>} tooltip={Desc.RemoveVulnFromDatabank} arrowPlacement="right" >Remove Vulnerability from Databank</CustButton>,
-<CustButton  startIcon={<DriveFolderUploadIcon />} tooltip={Desc.LoadVulnDatabank} arrowPlacement="right" >Load Vulnerability Databank</CustButton>,
-<CustButton  startIcon={<SaveIcon />} tooltip={Desc.SaveVulnDatabank} arrowPlacement="right" >Save Vulnerability Databank</CustButton>,
-<CustButton  startIcon={<ImportExportIcon />} tooltip={Desc.ImportVulnDatabank} arrowPlacement="right" >Import Vulnerability Databank</CustButton>
-];
-
-const AuditLog = [<CustButton  startIcon={<ImportExportIcon />} tooltip={Desc.ImportAuditLog} arrowPlacement="right" >Import Audit Log</CustButton>,];
-
-
-
-
 function App() {  
-  const [value, setValue] = React.useState('');
+  const [value, setValue] = React.useState(0);
 
-
-  const [page, setPage] = React.useState(0);
-
-  const handleChange = (event, newValue) => {
-    setValue(newValue);};
+  const handleChange = (event, newValue) => {setValue(newValue);};
 
   return(
   <React.Fragment>
@@ -81,20 +39,15 @@ function App() {
         flexGrow: 1}}>
 
 {/* This section of code handles the buttons displayed on the left sidebar */}
-
       <Grid item xs>
-        <CustButtonGroup Num={0} value={value} group={ButtonsReport}>Report Options</CustButtonGroup>
-        <CustButtonGroup Num={1} value={value} group={ButtonsVulnDatabank}>Vulnerability Options</CustButtonGroup>
-        <CustButtonGroup Num={2} value={value} group={AuditLog}>Audit Options</CustButtonGroup>
+        <CustButtonGroup Num={0} value={value} group={Side.ButtonsReport}>Report Options</CustButtonGroup>
+        <CustButtonGroup Num={1} value={value} group={Side.ButtonsVulnDatabank}>Vulnerability Options</CustButtonGroup>
+        <CustButtonGroup Num={2} value={value} group={Side.AuditLog}>Audit Options</CustButtonGroup>
       </Grid>
 
 {/* This section of code handles the tabs that appear for creating new headings! */}
-
       <Grid item xs={10}>
-
-        
         <CustTabPanel value={value} index={0}>
-
            <CustForm heading='Heading' headingTitle="Enter Section Heading" description="SectionDescription" descriptionTitle="Enter brief description of what this section is about" /> 
 
            <CustSection >Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
@@ -103,29 +56,18 @@ function App() {
     Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</CustSection> 
            <CustSection />
            <CustSection />
-           <CustSection />
-           <CustSection />
-           <CustSection />
-           <CustSection />
-    
         </CustTabPanel>
 
         <CustTabPanel value={value} index={1}> 
             <CustForm heading='Vuln' headingTitle="Enter Vulnerability Name" description="VulnDescription" descriptionTitle="Enter brief description of the vulnerability/Security concern" /> 
 
-            <CustSection /> 
         </CustTabPanel>
 
         <CustTabPanel value={value} index={2}> 
             <CustForm heading='Audit' headingTitle="Enter Audit Log" description="AuditDescription" descriptionTitle="Enter brief description of what this audit log contains" /> 
 
-            <CustSection /> 
         </CustTabPanel>
-
       </Grid>
-
-
-    
     </Grid>        
   </React.Fragment>
   );}
